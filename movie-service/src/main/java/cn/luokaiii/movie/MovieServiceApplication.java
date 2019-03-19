e3b9b05edd0691efdad4d22996bc1ca740f18f6a
罗凯 <17301858781@163.com>
2019-03-19 13:18:24 +0800

    todo 集群环境下session一致性问题

--- movie-service/src/main/java/cn/luokaiii/movie/MovieServiceApplication.java
+++ movie-service/src/main/java/cn/luokaiii/movie/MovieServiceApplication.java
@@ -1,4 +1,4 @@
-package cn.luokaiii.user;
+package cn.luokaiii.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +9,16 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @SpringBootApplication
-public class UserServiceApplication {
+public class MovieServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(MovieServiceApplication.class, args);
     }
 
+    // todo 这里有一个session一致性的问题
     @GetMapping("/")
     public String hello(HttpSession session) {
         return String.format("<h1>Hello World! %s</h1>", session.getId());
     }
+
 }
