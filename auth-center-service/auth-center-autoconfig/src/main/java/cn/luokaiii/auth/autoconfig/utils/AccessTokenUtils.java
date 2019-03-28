f1a20d5ca674950ea07813368c8056d7f742c2a7
罗凯 <17301858781@163.com>
2019-03-28 17:29:58 +0800

    完善Token工具类

--- auth-center-service/auth-center-autoconfig/src/main/java/cn/luokaiii/auth/autoconfig/utils/AccessTokenUtils.java
+++ auth-center-service/auth-center-autoconfig/src/main/java/cn/luokaiii/auth/autoconfig/utils/AccessTokenUtils.java
@@ -1,8 +1,11 @@
 package cn.luokaiii.auth.autoconfig.utils;
 
 import cn.luokaiii.auth.api.pojo.Constant;
+import cn.luokaiii.user.api.model.ModuleResources;
+import cn.luokaiii.user.api.model.MovieRole;
 import cn.luokaiii.user.api.model.MovieUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -10,6 +13,7 @@ import org.springframework.security.oauth2.provider.authentication.TokenExtracto
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 public class AccessTokenUtils {
 
@@ -22,11 +26,29 @@ public class AccessTokenUtils {
     @Autowired
     private TokenExtractor tokenExtractor;
 
+    @Autowired
+    private RedisTemplate<String, MovieRole> roleTemplate;
+
+    @Autowired
+    private RedisTemplate<String, ModuleResources> resourcesTemplate;
+
     /**
      * 从token获取用户信息
      */
     public MovieUser getUserInfo() {
         return (MovieUser) getAccessToken().getAdditionalInformation().get(Constant.USER_INFO);
+    }
+
+    public List<MovieRole> getRoleInfo() {
+        String userId = getUserInfo().getId();
+        long size = roleTemplate.opsForList().size(userId);
+        return roleTemplate.opsForList().range(userId, 0, size);
+    }
+
+    public List<ModuleResources> getMenuInfo() {
+        String key = getUserInfo().getId() + "-menu";
+        long size = resourcesTemplate.opsForList().size(key);
+        return resourcesTemplate.opsForList().range(key, 0, size);
     }
 
     private OAuth2AccessToken getAccessToken() throws AccessDeniedException {
