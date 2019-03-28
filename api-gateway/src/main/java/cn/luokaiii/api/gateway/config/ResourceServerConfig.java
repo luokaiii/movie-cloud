de30b925ebf9a3cb0b9fa0e389db196f9196cd7c
罗凯 <17301858781@163.com>
2019-03-28 18:07:12 +0800

    API Gateway 的安全过滤，过滤掉Actutor的安全检查

--- api-gateway/src/main/java/cn/luokaiii/api/gateway/config/ResourceServerConfig.java
+++ api-gateway/src/main/java/cn/luokaiii/api/gateway/config/ResourceServerConfig.java
@@ -1,4 +1,18 @@
 package cn.luokaiii.api.gateway.config;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/css/**", "/js/**", "/favicon.ico", "/webjars/**", "/images/**",
+                        "/hystrix.stream/**", "/info", "/error", "/health", "/env", "/metrics", "/trace", "/dump",
+                        "/actuator", "/actuator/**",
+                        "/jolokia", "/configprops", "/activiti", "/logfile", "/refresh", "/flyway", "/liquibase", "/loggers");
+    }
 }
